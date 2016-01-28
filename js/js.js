b165371b2d5bd9805587a8317220d9d4ad446fea
jbabrams2 <jbabrams2@gmail.com>
2016-01-28 14:56:43 -0600

    add bottom nav with button,contact-form overlay and modal, and product overlay

---
+++ js/js.js
@@ -0,0 +1,15 @@
+function show(target,target2){
+    document.getElementById(target).style.display = 'block';
+    document.getElementById(target2).style.display = 'block';
+    }
+
+function hide(target,target2) {
+  document.getElementById(target).style.display = 'none';
+  document.getElementById(target2).style.display = 'none';
+}
+
+
+function showProduct(target,target2){
+    document.getElementById(target).style.display = 'block';
+    document.getElementById(target2).style.display = 'block';
+    }
