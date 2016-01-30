40992dba9a76abef4f21792a8efe5c94524a6fd7
jbabrams2 <jbabrams2@gmail.com>
2016-01-30 10:31:23 -0600

    add overlays per product and insert jquery animation test

--- js/js.js
+++ js/js.js
@@ -13,3 +13,24 @@ function showProduct(target,target2){
     document.getElementById(target).style.display = 'block';
     document.getElementById(target2).style.display = 'block';
     }
+
+function hideProduct(target,target2) {
+    document.getElementById(target).style.display = 'none';
+    document.getElementById(target2).style.display = 'none';
+    }
+
+function showImgOverlay(target){
+    document.getElementById(target).style.display = 'block';
+    }
+
+$('.img').hover(function(){
+  $('#img-container div').animate({left: '0px'});
+});
+
+$('.img').mouseout(function(){
+  $('#img-container div').animate({left: '-350px'});
+});
+
+function hideImgOverlay(target){
+    document.getElementById(target).style.display = 'none';
+    }
