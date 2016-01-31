62c720364321d829824dc340ab59ac042c1055c1
jbabrams2 <jbabrams2@gmail.com>
2016-01-31 13:15:00 -0600

    temporarily place nav in section while determining what's causing unintended partial stick.

--- js/js.js
+++ js/js.js
@@ -7,12 +7,11 @@ function hide(target,target2) {
   document.getElementById(target).style.display = 'none';
   document.getElementById(target2).style.display = 'none';
 }
-
-
-function showProduct(target){
-    document.getElementById(target).style.display = 'block';
-    }
-
-function hideProduct(target) {
-    document.getElementById(target).style.display = 'none';
-    }
+// 
+// $(window).scroll(function() {
+//   if ($(document).scrollTop() > 50) {
+//     $('#header').addClass('shrink');
+//   } else {
+//     $('#header').removeClass('shrink');
+//   }
+// });
