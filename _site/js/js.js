1f43c546fa3312bd5a175361a0d288e5bff12250
jbabrams2 <jbabrams2@gmail.com>
2016-02-01 14:07:30 -0600

    jekyll conversion across every page

---
+++ _site/js/js.js
@@ -0,0 +1,17 @@
+function show(target,target2){
+    document.getElementById(target).style.display = 'block';
+    document.getElementById(target2).style.display = 'block';
+    }
+
+function hide(target,target2) {
+  document.getElementById(target).style.display = 'none';
+  document.getElementById(target2).style.display = 'none';
+}
+// 
+// $(window).scroll(function() {
+//   if ($(document).scrollTop() > 50) {
+//     $('#header').addClass('shrink');
+//   } else {
+//     $('#header').removeClass('shrink');
+//   }
+// });
