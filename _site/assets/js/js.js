24cdda171e229b816374520aa97ad0f97881ac58
jbabrams2 <jbabrams2@gmail.com>
2016-02-01 21:54:11 -0600

    fix overlay pixel short

--- _site/assets/js/js.js
+++ _site/assets/js/js.js
@@ -7,11 +7,20 @@ function hide(target,target2) {
   document.getElementById(target).style.display = 'none';
   document.getElementById(target2).style.display = 'none';
 }
-// 
+//
 // $(window).scroll(function() {
 //   if ($(document).scrollTop() > 50) {
 //     $('#header').addClass('shrink');
 //   } else {
 //     $('#header').removeClass('shrink');
 //   }
+// });
+
+/*SAMPLE GRAYSCALE JQUERY*/
+// $("#uppdO").hover(function(){
+//     $("#test").addClass("grayscale");
+// });
+//
+// $("#uppdO").mouseout(function(){
+//     $("#test").removeClass("grayscale");
 // });
